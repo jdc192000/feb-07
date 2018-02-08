7af35b3467dbc6cf6dd42fbda9332822eb4c5656
Christian Rittmann <crittmann@hotmail.com>
2018-02-07 20:18:41 -0600

    cr added files

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# feb-07
-hangman game
+# Hangman-Game
---
+++ assets/javascript/game.js
@@ -0,0 +1,173 @@
+//pseudocode
+//===============================
+
+//Need a wordArray for computer to randomly choose from
+//take index of wordArray to get equal image from imageArray
+    //show image in html
+//Converts picked word into equal length of "_"
+
+//Need global variables
+
+//Need A function that takes user input: keypresses
+//Checks if user input is in random word
+//If user input is in random word reveals the letter
+    //+1 to counter
+    //if counter is equal to random word.length add 1 to wins
+        //updates wins on html
+//else user input is not in random word
+    //push userguess to letters guessed array
+    //-1 to guessesRemain
+    //updates guessesRemain on html
+    //if guessesRemain is equal to 0 add 1 to losses
+        //call reset function
+
+//Need a function that resets variables:guessesRemain and 
+//picks another word and switches images to the same as randomWord
+
+
+
+var wordArray = ["cardinals", "falcons", "ravens", "bills", "panthers", "bears"
+    , "bengals", "browns", "cowboys", "broncos", "lions", "packers", "texans", "colts"
+    , "jaguars", "chiefs", "chargers", "rams", "dolphins", "vikings", " patriots"
+    , "saints", "giants", "jets", "raiders", "eagles", "steelers", "fourtyniners"
+    , "seahawks", "buccaneers", "titans", "redskins"]
+
+var winCounter = 0;
+var guessesRemain = 13;
+var lettersGuessed = [];
+var imgArray = [];
+var answerArray = [];
+var letterCounter = 0;
+var userGuess = "";
+
+//Function to pick a random word from wordArray and convert to equal length "_"
+function pickAndConvertWord() {
+
+    // Picks a random word from the word array
+    var randomWord = wordArray[Math.floor(Math.random() * wordArray.length)];
+    console.log(randomWord);
+
+    // Convert randomWord to equal length of "_"
+    for (var i = 0; i < randomWord.length; i++) {
+        answerArray.push("_");
+    }
+
+    // Takes out commas of current word
+    document.getElementById("currentWord").innerHTML = answerArray.join(" ")
+}
+
+
+//function that starts when key is pressed
+document.onkeydown = function (event) 
+{
+
+    // sets the variable userGuess to the user key input
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
+    //checks if userGuess is in randomWord array
+    if (randomWord.indexOf(userGuess) >-1) 
+    {
+        for(i=0; i<randomWord.length; i++) 
+        {
+            if (randomWord[i] === userGuess) 
+            {
+                answerArray[i] = userGuess;
+                letterCounter++;
+                document.getElementById("currentWord").innerHTML = answerArray.join(" ");
+
+                if(letterCounter == randomWord.length) {
+                    winCounter++;
+                    document.getElementById("wins").innerHTML = winCounter;
+                    
+            }
+        }
+    } 
+    
+    //If userGuess != randomWord array then lowers guesses remain and pushes
+    //user guess to lettersGuessed array
+    else
+    {
+        guessesRemain--;
+        document.getElementById("guessesLeft").innerHTML = guessesRemain;
+        lettersGuessed.push(userGuess);
+        document.getElementById("lettersUsed").innerHTML = lettersGuessed;
+    }   
+
+
+}
+
+
+
+
+
+
+
+
+
+
+/*
+// Word array
+var wordArray = ["cardinals", "falcons", "ravens", "bills", "panthers", "bears"
+    , "bengals", "browns", "cowboys", "broncos", "lions", "packers", "texans", "colts"
+    , "jaguars", "chiefs", "chargers", "rams", "dolphins", "vikings", " patriots"
+    , "saints", "giants", "jets", "raiders", "eagles", "steelers", "fourtyniners"
+    , "seahawks", "buccaneers", "titans", "redskins"];
+
+var randomWord = wordArray[Math.floor(Math.random() * wordArray.length)];
+
+var winCounter = 0;
+var guessesRemain = 13;
+var lettersGuessed = [];
+var imgArray = [];
+var answerArray = [];
+var s;
+var userGuess = "";
+
+
+
+//get users guess
+document.onkeyup = function(event) {
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+    guessesRemain--;
+ 
+
+//check users guess for equal letters in current word
+
+    //if guess === letter reveal letter in current word
+    if (userGuess === randomWord[i]) {
+        answerArray[i] = userGuess;
+        
+    } 
+    //else guess !== letter push guess to lettersGuessed array
+    else {
+        lettersGuessed.push(userGuess);
+        document.getElementById("lettersUsed").innerHTML = lettersGuessed;
+        
+    }
+}  
+
+    
+
+//If current word is revealed then winCounter++ and pick another word
+function endGame() {
+    if (answerArray !== "_") {
+        winCounter++;
+        document.getElementById("#wins") = winCounter;
+        // calls function that randomly generates new word;
+    } 
+
+    else if (answerArray === "_") {
+        //alert player to guess another letter
+        alert("Please guess another letter")
+    }
+
+    else {
+        guessesRemain === 0;
+        //alert player they lost and show randomWord
+        alert("You have ran out of guesses" + randomWord)
+        //call function that randomly generates a new word
+    }
+}
+*/
+
+function xx() 
